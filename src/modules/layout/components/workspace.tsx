d3f3fca053804480c32f3242f04b2ef6b39f879d
Mr. AnshuHemu <anshukatariya4488@gmail.com>
2025-12-17 22:23:27 +0530

    Workspaces Implementation..

--- src/modules/layout/components/workspace.tsx
+++ src/modules/layout/components/workspace.tsx
@@ -1,21 +1,87 @@
 "use client";
 import { Button } from "@/components/ui/button";
 import { Hint } from "@/components/ui/hint";
-import { Select } from "@/components/ui/select";
-import { User } from "lucide-react";
-import React from "react";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { useWorkspaces } from "@/modules/workspace/hooks/workspace";
+import { Loader, Plus, User } from "lucide-react";
+import React, { useEffect, useState } from "react";
+import { useWorkspaceStore } from "../store";
+import { Separator } from "@/components/ui/separator";
+import CreateWorkspace from "./create-workspace";
 
 const Workspace = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const { data: workspaces, isLoading } = useWorkspaces();
+
+  const { selectedWorkspace, setSelectedWorkspace } = useWorkspaceStore();
+
+  useEffect(() => {
+    if (workspaces && workspaces.length > 0 && !selectedWorkspace) {
+      setSelectedWorkspace(workspaces[0]);
+    }
+  }, [workspaces, selectedWorkspace, setSelectedWorkspace]);
+
+  if (isLoading) {
+    return <Loader className="animate-spin size-4 text-indigo-400" />;
+  }
+
+  if (!workspaces || workspaces.length === 0) {
+    return (
+      <div className="font-semibold text-indigo-400">No Workspace Found</div>
+    );
+  }
+
   return (
     <>
       <Hint label="Change Workspace">
-        <Button className="border border-indigo-400 bg-indigo-400/10 hover:bg-indigo-400/20 text-indigo-400 hover:text-indigo-300 flex flex-row items-center space-x-1">
-          <User className="size-4 text-indigo-400" />
-          <span className="text-sm text-indigo-400 font-semibold">
-            Personal Workspace
-          </span>
-        </Button>
+        <Select
+          value={selectedWorkspace?.id}
+          onValueChange={(id) => {
+            const ws = workspaces.find((w) => w.id === id);
+            if (ws) setSelectedWorkspace(ws);
+          }}
+        >
+          <SelectTrigger className="border border-indigo-400 bg-indigo-400/20 text-indigo-400 hover:text-indigo-300 flex flex-row items-center space-x-1">
+            <User className="size-4 text-indigo-400" />
+            <span className="text-sm text-indigo-400 font-semibold">
+              <SelectValue placeholder="Select Workspace" />
+            </span>
+          </SelectTrigger>
+
+          <SelectContent>
+            {workspaces.map((ws) => (
+              <SelectItem key={ws.id} value={ws.id}>
+                {ws.name}
+              </SelectItem>
+            ))}
+            <Separator className="my-1" />
+            <div className="p-2 flex flex-row justify-between items-center w-full">
+              <span className="text-sm font-semibold text-zinc-600">
+                My Workspaces
+                <Button
+                  size={"icon"}
+                  variant={"outline"}
+                  onClick={() => setIsModalOpen(true)}
+                >
+                  <Plus size={16} className="text-indigo-400" />
+                </Button>
+              </span>
+            </div>
+          </SelectContent>
+        </Select>
       </Hint>
+
+      <CreateWorkspace
+        isModalOpen={isModalOpen}
+        setIsModalOpen={setIsModalOpen}
+      />
     </>
   );
 };
